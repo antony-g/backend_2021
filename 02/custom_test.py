0d8c4d4c5ff3822517fc63eb39d1c49823fc7b50
antony-g <antony.gladky@gmail.com>
2021-03-22 17:13:32 +0300

    fool-proof commit

--- 02/custom_test.py
+++ 02/custom_test.py
@@ -15,29 +15,20 @@ class TestClass(metaclass=Custom):
         "Empty func"
 
 
-class VarNameTest(unittest.TestCase):
+class MyTestCase(unittest.TestCase):
     """Testing class variable naming"""
     def test_var(self):
         "Docstring"
         self.assertTrue(hasattr(TestClass, 'custom_some_variable'))
 
-
-class MethodNameTest(unittest.TestCase):
-    """Testing class methods naming"""
     def test_method(self):
         "Docstring"
         self.assertTrue(hasattr(TestClass, 'custom_some_func'))
 
-
-class BuiltinNameTest(unittest.TestCase):
-    """Testing built-in (magic) methods naming"""
     def test_builtin(self):
         "Docstring"
         self.assertTrue(hasattr(TestClass, '__doc__'))
 
-
-class ClassNameTest(unittest.TestCase):
-    """Testing class naming"""
     def test_class(self):
         "Docstring"
         self.assertEqual(TestClass.__name__, 'CustomTestClass')
