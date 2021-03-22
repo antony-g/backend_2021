f5a476420f5bfcb64c9800f0e73076f00dc15ba6
antony-g <antony.gladky@gmail.com>
2021-03-22 12:14:54 +0300

    fool-proof commit

--- 02/custom_test.py
+++ 02/custom_test.py
@@ -17,46 +17,30 @@ class TestClass(metaclass=Custom):
 
 class VarNameTest(unittest.TestCase):
     """Testing class variable naming"""
-    def test_positive(self):
+    def test_var(self):
         "Docstring"
         self.assertTrue(hasattr(TestClass, 'custom_some_variable'))
-
-    def test_negative(self):
-        "Docstring"
-        self.assertFalse(hasattr(TestClass, 'some_variable'))
 
 
 class MethodNameTest(unittest.TestCase):
     """Testing class methods naming"""
-    def test_positive(self):
+    def test_method(self):
         "Docstring"
         self.assertTrue(hasattr(TestClass, 'custom_some_func'))
-
-    def test_negative(self):
-        "Docstring"
-        self.assertFalse(hasattr(TestClass, 'some_func'))
 
 
 class BuiltinNameTest(unittest.TestCase):
     """Testing built-in (magic) methods naming"""
-    def test_positive(self):
+    def test_builtin(self):
         "Docstring"
         self.assertTrue(hasattr(TestClass, '__doc__'))
-
-    def test_negative(self):
-        "Docstring"
-        self.assertFalse(hasattr(TestClass, 'custom__doc__'))
 
 
 class ClassNameTest(unittest.TestCase):
     """Testing class naming"""
-    def test_positive(self):
+    def test_class(self):
         "Docstring"
         self.assertEqual(TestClass.__name__, 'CustomTestClass')
-
-    def test_negative(self):
-        "Docstring"
-        self.assertNotEqual(TestClass.__name__, 'TestClass')
 
 
 if __name__ == '__main__':
